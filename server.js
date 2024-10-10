751e27395f6101dfe5c6c339203a8cff4b00b568
Vladimir Klimkin <oniwik61@gmail.com>
2024-10-10 10:34:35 +0300

    Add connection to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
 
 // Routes
 app.get('/', (req, res) => {
